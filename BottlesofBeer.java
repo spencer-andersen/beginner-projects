a9b2d3db5b3c0318160d8e48dbaca16810864d2b
Spencer <email-blocked>
2015-09-25 19:17:08 -0400

    First commitment

---
+++ BottlesofBeer.java
@@ -0,0 +1,31 @@
+
+//prints out the lyrics to the classic song, "99 bottles of Beer on the Wall."
+ public class BottlesofBeer {
+
+     public static void main(String[] args) {
+     	  int beer = 99;
+     	  String bottles = "bottles";
+	  while(beer > 0) {
+	  
+	  if (beer == 1) { 
+	  
+	  bottles = "bottle";
+	  }
+
+	   System.out.println(beer + bottles + " of beer on the wall " + beer +  bottles + " of beer,\n");
+	   System.out.println("Take one down, pass it around, \n");
+	   beer --;
+	 
+
+	  if (beer > 0) {
+	  
+	   System.out.println(beer + bottles + "bottles of beer on the wall. \n");
+	  }
+     
+     else { //there are no more bottles left at all
+          System.out.println("No more bottles of beer on the wall, no more bottles of beer,\n");
+	  System.out.println("Go to the store and buy some more! 99 Bottles of beer on the wall!");
+    }
+   }
+}
+}
