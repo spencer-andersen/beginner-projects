f64ee0c0b5403873c1ad207bc4d83ca2f8f6aa01
spencer-andersen <spencer.t.andersen@gmail.com>
2015-09-29 19:41:02 -0400

    Work in progress

---
+++ piglatin.py
@@ -0,0 +1,19 @@
+def piglatin(string):
+  digraphs = ['bl','br','ch','cl','cr','dr','fl','fr','gl','gr','pl','pr','sc',
+'sh','sk','sl','sm','sn','sp','st','sw','th','tr','tw','wh','qu','wr']
+  trigraphs = ['sch', 'scr', 'str', 'sph', 'spl', 'squ', 'thr']
+  vowels = ['a','e','i','o','u']
+  s = []
+  split_string  = string.split()
+  for word in split_string:
+    if word[0] in vowels: 
+     new = word + 'yay'
+    elif word[:2] in digraphs:
+       new = word[2:] + word[:2] + 'ay'
+    elif word[:3] in trigraphs:   
+       new = word[3:] + word[:3] + 'ay'
+    else:
+       new = word[1:] + word[:1] + 'ay'
+    s.append(new)
+  s = ' '.join(s)
+  return s
