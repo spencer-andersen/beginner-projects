10fc7f258c04677d354ae63444459ca639fafe43
spencer-andersen <spencer.t.andersen@gmail.com>
2015-09-29 19:47:21 -0400

    Formatted to look cleaner

--- BottlesofBeer.java
+++ BottlesofBeer.java
@@ -6,25 +6,17 @@
      	  int beer = 99;
      	  String bottles = " bottles";
 	  while(beer > 0) {
-	
-
-	 
-	   System.out.println(beer + " " + bottles + " of beer on the wall ");
-	   System.out.println(beer +  " " + bottles + " of beer,\n");
-	   System.out.println("Take one down, pass it around, \n");
+	System.out.println(beer + " " + bottles + " of beer on the wall ");
+	System.out.println(beer +  " " + bottles + " of beer,\n");
+	System.out.println("Take one down, pass it around, \n");
 	   beer --;
-	   
-	      if (beer == 1) { 
-	  
+	if (beer == 1) { 
 	  bottles = " bottle";
 	  }
 	  if (beer > 0) {
 	  
 	   System.out.println(beer + " "+ bottles + " of beer on the wall. \n");
 	  }
-        	  
-        	  
-
      else { //there are no more bottles left at all
           System.out.println("No more bottles of beer on the wall\n");
           System.out.println("No more bottles of beer on the wall, no more bottles of beer,");
