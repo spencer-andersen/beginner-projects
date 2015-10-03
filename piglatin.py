5fe6b062cae7b6260c1fdeeb44c88859280c6ea0
Spencer <spencer@users.noreply.github.com>
2015-10-03 13:54:45 -0400

    Added feature that recognizes punctuation

--- piglatin.py
+++ piglatin.py
@@ -4,6 +4,8 @@ def piglatin(string):
   trigraphs = ['sch', 'scr', 'str', 'sph', 'spl', 'squ', 'thr']
   vowels = ['a','e','i','o','u']
   s = []
+  punctuation = ['?','!','.',',']
+  punc = [len(word)-1:] #as punctuation is at the end of words, this notes the punctuation
   split_string  = string.split()
   for word in split_string:
     if word[0] in vowels: 
@@ -11,6 +13,8 @@ def piglatin(string):
     elif word[:2] in digraphs or word[:3] in trigraphs:   
        new = word[2:] + word[:2] + 'ay'
        new = word[3:] + word[:3] + 'ay'
+    elif punc in punctuation:
+       new = word.strip(punc) + word[1:] + word[:1] + punc
     else:
        new = word[1:] + word[:1] + 'ay'
     s.append(new)
