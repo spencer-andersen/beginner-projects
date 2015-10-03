97af303b8e915ca0a2b884ff21c5c375dffc14f5
Spencer <spencer-andersen@users.noreply.github.com>
2015-10-03 15:09:19 -0400

    Decided to simplify (no need to overcomplicate things

--- piglatin.py
+++ piglatin.py
@@ -3,18 +3,15 @@ def piglatin(string):
 'sh','sk','sl','sm','sn','sp','st','sw','th','tr','tw','wh','qu','wr']
   trigraphs = ['sch', 'scr', 'str', 'sph', 'spl', 'squ', 'thr']
   vowels = ['a','e','i','o','u']
-  s = []
-  punctuation = ['?','!','.',',']
-  punc = [len(word)-1:] #as punctuation is at the end of words, this notes the punctuation
+  s = []:
   split_string  = string.split()
   for word in split_string:
     if word[0] in vowels: 
      new = word + 'yay'
-    elif word[:2] in digraphs or word[:3] in trigraphs:   
+    elif word[:2] in digraphs:   
        new = word[2:] + word[:2] + 'ay'
+    elif word[3:] in trigraphs:
        new = word[3:] + word[:3] + 'ay'
-    elif punc in punctuation:
-       new = word.strip(punc) + word[1:] + word[:1] + punc
     else:
        new = word[1:] + word[:1] + 'ay'
     s.append(new)
